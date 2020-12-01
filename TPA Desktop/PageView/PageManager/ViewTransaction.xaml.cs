c03e13264e39d4cd8ec3f46ea6453579d99323a5
Vincent-Benedict <74532088+Vincent-Benedict@users.noreply.github.com>
2020-12-01 22:23:20 +0700

    second-attempt

--- TPA Desktop/PageView/PageManager/ViewTransaction.xaml.cs
+++ TPA Desktop/PageView/PageManager/ViewTransaction.xaml.cs
@@ -13,8 +13,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using ExcelLibrary.CompoundDocumentFormat;
-using ExcelLibrary.SpreadSheet;
 using TPA_Desktop.MVVM;
 using Microsoft.Office.Interop.Excel;
 
